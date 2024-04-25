0b9601f18b0e0ce2feb88d2f8b04fb691bd6a5d9
Louiscars190 <louiscars170@gmail.com>
2024-04-25 00:23:53 -0400

    Works, but doesn't wait 10 seconds and completely relies on the sensors

--- CodeforNoSlaveArduino.ino
+++ CodeforNoSlaveArduino.ino
@@ -21,30 +21,17 @@ unsigned long moveStartTime = 0; // this will save the time (millis()) when we s
 unsigned long move2StartTime = 0;
 
 /*Switch-State Machine*/
-enum Bridgestate {IDLE, MOVING};  // Makes an Enumeration containing IDLE and MOVING to be used in the state machine.
-     Bridgestate Bridgestate = IDLE; // Starts the code with Bridgestate initially set to IDLE
+enum Bridgestate {IDLE, MOVING};
+     Bridgestate Bridgestate = 0;  // Makes an Enumeration containing IDLE and MOVING to be used in the state machine. // Starts the code with Bridgestate initially set to IDLE
 bool Bridgedown = true; // Might not need
 
 /*Non Blocking Timers*/
 BlockNot strobetimer(1000);
-BlockNot SerialSonicTimer(750);
+BlockNot SerialSonicTimer(1500);
+BlockNot SerialSonic2Timer(1500);
 BlockNot SerialBridgeTimer(2000); // Updates Bridge Status every two seconds
-BlockNot bridgetimer(10000);
+BlockNot bridgetimer(4000);
 BlockNot traffictimer(500);
-
-/*
-unsigned long currentmillis = millis(); // Starts the clock for current millis, can use it to update various differnt 'previousmillis' variables
-unsigned long previousmillis = 0;
-const long strobeint = 1000;        // Change the interval if you want to change how fast the strobes blink
-
-const long bridgeint = 10000;     // How long the bridge will stay down for before going back up
-unsigned long bridgeprevmill = 0; // Initilizating previous millisecond variables for bridge so that it stays down for 10 seconds
-
-const long serialupdateint = 950;
-
-const long trafficint = 500;        // Initializing constant interval for traffic light
-unsigned long trafficprevmill = 0;  // Initializing previous millisecond variables for green traffic interval
-*/
 
 void setup() 
 {
@@ -65,62 +52,100 @@ void setup()
   Serial.println("Done Initilization, Ready for cars to come!"); // Outputs 'Done Initilization, Ready for cars to come!' to the Serial Monitor on Arduino IDE
 }
 
-void loop()
-{ 
-  Masterdistance = masterdistanceread(); // Update variable with current distance read from sensor
-  Slavedistance = slavedistanceread();   // ||
+void loop() 
+{
+  Bridgestate = IDLE;
 
-   if(strobetimer.TRIGGERED) 
+  Masterdistance = masterdistanceread();
+  Slavedistance = slavedistanceread();
+
+  sonicMaster.startAsync(200000);
+  while(!sonicMaster.isFinished()) 
   {
-    LEDSTATE = !LEDSTATE;
-    digitalWrite(StrobePin, LEDSTATE); // Blinks Strobes every second
-  }
-  switch (Bridgestate) // IDLE and MOVING, two states of the Bridge system
-  {
-    case IDLE: // Trafffic Light is Red, Listening for Car. If car there, change state to moving.
-      digitalWrite(RedTrafficPin, HIGH);   // Turns on Red Traffic Lights
-      digitalWrite(GreenTrafficPin, LOW);  // Turns off Green Traffic Lights
-      if (SerialBridgeTimer.TRIGGERED)
-      {
-        Serial.print("Bridge State: ");      // Outputs Bridge State to Serial Monitor for Debugging purposes
-        Serial.println(Bridgestate);
-      }
-      if (Masterdistance <= 4 || Slavedistance <= 4) // USED MORE THAN TO TEST STATE #0
-      {
+    stepper.run();
+    stepper2.run();
+    int stepsLeft = stepper.getStepsLeft();
+    int steps2Left = stepper2.getStepsLeft();
+
+    if(strobetimer.TRIGGERED) 
+    {
+      LEDSTATE = !LEDSTATE;
+      digitalWrite(StrobePin, LEDSTATE); // Blinks Strobes every second
+    }
+    switch (Bridgestate)
+    {
+      case IDLE:
+        digitalWrite(RedTrafficPin, HIGH);   // Turns on Red Traffic Lights
+        digitalWrite(GreenTrafficPin, LOW);  // Turns off Green Traffic Lights
+        if (SerialBridgeTimer.TRIGGERED)
+        {
+         Serial.print("Bridge State: ");      
+         Serial.println(Bridgestate);
+        }
+        if (Masterdistance < 4 || Slavedistance < 4) 
+        {
+          Bridgestate = MOVING;
+        }
+        break;
+      case MOVING:
+        if (SerialBridgeTimer.TRIGGERED)
+        {
+          Serial.print("Bridge State: ");
+          Serial.println(Bridgestate);
+        }
+          flashinglights();
+        /*Moving Down*/
+          moveClockwise = !moveClockwise; // reverse direction
+          stepper.newMoveDegrees (moveClockwise, 180); // move 180 degrees from current position
+          moveStartTime = millis(); // reset move start time
+          
+          stepper2.newMoveDegrees (!moveClockwise, 180); // move 180 degrees from current position
+          move2StartTime = millis(); // reset move start time
+        bridgetimer.start(WITH_RESET);
+        if (bridgetimer.TRIGGERED)
+        {
+          moveClockwise = !moveClockwise; // reverse direction
+          stepper.newMoveDegrees (!moveClockwise, 180); // move 180 degrees from current position
+          moveStartTime = millis(); // reset move start time
+
+          stepper2.newMoveDegrees (moveClockwise, 180); // move 180 degrees from current position
+          move2StartTime = millis(); // reset move start time
+        }
+        if (stepsLeft || steps2Left) 
+        {
         Bridgestate = MOVING;
-      }
-      break;
-    case MOVING:
-      if(SerialBridgeTimer.TRIGGERED)
-      {
-        Serial.print("Bridge State: "); // Tells us the state of the bridge on the serial monitor (Debugging purposes)
-        Serial.println("Moving");    // Prints the current case of 'Bridgestate'
-      }
-      flashinglights(); // Flashes the RED and GREEN LEDS from variables 'GreenTrafficPin' and 'RedTrafficPin' Only when moving Green and Red pins
-      
-      //{
-      //  Bridgestate = IDLE;
-      //}
-      //Bridgestate = IDLE;
-      break;
+        } 
+        else 
+        {
+        Bridgestate = IDLE;
+        }
+        break;
+    }
   }
 }
+
 
 // Reads the distance measured by the ultrasonic and can output the number to serial.
 float masterdistanceread() // Outputs the distance of a master rangefinder as a function 
 {
-  Masterdistance = sonicMaster.getDist_cm(1);
-  //Serial.print("Distance of Master: "); // Outputs Distance in centimeters to the Serial Monitor
-  //Serial.println(Masterdistance);
+  Masterdistance = sonicMaster.getDist_cm(0);
+  if(SerialSonicTimer.TRIGGERED)
+  {
+    Serial.print("Distance of Master: "); // Outputs Distance in centimeters to the Serial Monitor
+    Serial.print(Masterdistance );
+  }
   return(Masterdistance);
 }
 
 // Reads the distance measured by the ultrasonic and can output the number to serial.
 float slavedistanceread() // Outputs the distance of slave rangefinder as a function 
 {
-  Slavedistance = sonicMaster.getDist_cm(2);
-  //Serial.print("Distance of Slave: ");
-  //Serial.println(Slavedistance);
+  Slavedistance = sonicMaster.getDist_cm(1);
+  if (SerialSonic2Timer.TRIGGERED)
+  {
+    Serial.print("Distance of Slave: ");
+    Serial.println(Slavedistance);
+  }
   return(Slavedistance);
 }
 
@@ -134,22 +159,4 @@ void flashinglights()
   return; 
 }
 
-/*void movebridge()
-{
-  
-  
-  if (bridgetimer.TRIGGER) // Basically does nothing for 10 seconds 
-  {
-    Bridgedown = true;
-    Serial.println("The Bridge is currently down, please proceed!");
-    openbarrier();
-  }
-  Bridgedown = false; // Right after the wait is over
-  closebarrier();   // Closes the barriers on the road which is default
-  // Move stepper to up
-  return; // Moves Bridge
-}
-*/
 /*--------------END-----------*/
-
-
